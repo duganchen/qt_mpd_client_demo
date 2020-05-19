9a0e60a1a04c5010eeae20d48491d1136b318ff6
Dugan Chen <thedoogster@gmail.com>
2020-05-19 11:56:41 -0700

    Unit test can spin up an MPD instance

--- tests/tst_testcontroller.cpp
+++ tests/tst_testcontroller.cpp
@@ -1,34 +1,91 @@
-#include <QtTest>
 #include <QCoreApplication>
-
-// add necessary includes here
+#include <QDebug>
+#include <QtTest>
+#include <mpd/client.h>
 
 class TestController : public QObject
 {
-	Q_OBJECT
+    Q_OBJECT
 
 public:
-	TestController();
-	~TestController();
+    TestController();
+    ~TestController();
 
 private slots:
-	void test_case1();
-
+    void test_spinUpMPD();
 };
 
 TestController::TestController()
 {
-
 }
 
 TestController::~TestController()
 {
-
 }
 
-void TestController::test_case1()
+void TestController::test_spinUpMPD()
 {
+    QFile templateFile{"resources/mpd.conf"};
+    QVERIFY(templateFile.open(QIODevice::ReadOnly | QIODevice::Text));
+    QTextStream in{&templateFile};
+    QString tmplate{in.readAll()};
+    tmplate = tmplate.arg(QCoreApplication::applicationDirPath() + "/resources/Music");
+    QTemporaryDir dir;
+    QVERIFY(dir.isValid());
+    auto tempPath = dir.path();
 
+    QDir().mkdir(tempPath + "/playlists");
+    tmplate = tmplate.arg(tempPath);
+
+    auto confPath = tempPath + "/mpd.conf";
+    QFile confFile{confPath};
+    QVERIFY(confFile.open(QIODevice::WriteOnly | QIODevice::Text));
+    QTextStream confStream(&confFile);
+    confStream << tmplate;
+
+    confFile.close();
+
+    /* A sample conf would be:
+     music_directory		"/home/dugan/Documents/qt_mpd_client_demo/tests/build/resources/Music"
+     playlist_directory		"/tmp/TestController-XFQcRT/playlists"
+    db_file			"/tmp/TestController-XFQcRT/database"
+    log_file			"/tmp/TestController-XFQcRT/log"
+    pid_file			"/tmp/TestController-XFQcRT/pid"
+    port				"6601"
+    */
+
+    QProcess mpd;
+    QStringList args;
+    args.append(confPath);
+    mpd.start("mpd", args);
+    mpd.waitForFinished();
+
+    auto conn = mpd_connection_new("localhost", 6601, 0);
+
+    if (!mpd_search_db_tags(conn, MPD_TAG_TITLE))
+    {
+        qDebug() << mpd_connection_get_error_message(conn);
+    }
+
+    if (!mpd_search_commit(conn))
+    {
+        qDebug() << mpd_connection_get_error_message(conn);
+    }
+
+    struct mpd_pair *pair = nullptr;
+    while ((pair = mpd_recv_pair_tag(conn, MPD_TAG_TITLE)) != nullptr)
+    {
+        qDebug() << pair->value;
+        mpd_return_pair(conn, pair);
+    }
+
+    mpd_connection_free(conn);
+
+    args.clear();
+    args.append("--kill");
+    args.append(confPath);
+    mpd.start("mpd", args);
+    mpd.waitForFinished();
 }
 
 QTEST_MAIN(TestController)
